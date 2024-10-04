44ee50a355726279237a74f47c511888f343fe88
Kyle Hollars <kylemhollars@gmail.com>
2024-10-04 13:47:43 -0400

    fixed open_page command

--- frontend/input_process.py
+++ frontend/input_process.py
@@ -17,8 +17,7 @@ class Processor:
 
     ### Actions ###
     def open_page(self, id: int):
-
-        subprocess.run(f"expolorer.exe https://eln.ddomlab.org/database.php?mode=view&id={id}")
+        subprocess.run(f"explorer.exe https://eln.ddomlab.org/database.php?mode=view&id={id}")
 
     def archive_item(self, id: int):
         self.rm.change_item(id, {"action": "archive"})
