daadfbb8ea1a26805f1df56174908adce59d8cd0
12buntu <kylemhollars@gmail.com>
2024-10-09 00:30:34 -0400

    added setup.py files

---
+++ frontend/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Ddomlab-ELN-Automation-Backend",
+    version="0.1.0",
+    packages=find_packages(),
+    author="Kyle Hollars",
+    author_email="kmhollar@ncsu.edu",
+    python_requires=">=3.11",
+)
