dd3f977899f5721e1b7f79b8d682823105debfe2
12buntu <kylemhollars@gmail.com>
2024-11-03 17:15:34 -0500

    refresh on change working.

--- subapplications/add_resource.py
+++ subapplications/add_resource.py
@@ -8,18 +8,30 @@ class Add_Resource_Window(tk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
         self.parent = parent
-        self.scrollframe = self
         self.dropdown = Category_Dropdown(self)
         self.dropdown.pack()
 
-        self.cas_label = tk.Label(self, text="Search by CAS (or name if CAS unavailable):")
-        self.cas_label.pack(pady=10)
-        self.cas_entry = tk.Entry(self)
-        self.cas_entry.bind("<Return>", self.fill_info)
-        self.search_button = tk.Button(self, text="Search", command=self.fill_info)
-        self.cas_entry.pack(pady=5)
-        self.search_button.pack(pady=0)
-        self.pack(side="left")
+        self.draw_items()
+
+
+
+    def draw_items(self):
+        # clear all widgets in the frame
+        for widget in self.winfo_children():
+            # exclude self.dropdown
+            if widget != self.dropdown:
+                widget.destroy()
+
+
+        if self.dropdown.clicked.get() != "Instrument":
+            self.cas_label = tk.Label(self, text="Search by CAS (or name if CAS unavailable):")
+            self.cas_label.pack(pady=10)
+            self.cas_entry = tk.Entry(self)
+            self.cas_entry.bind("<Return>", self.fill_info)
+            self.search_button = tk.Button(self, text="Search", command=self.fill_info)
+            self.cas_entry.pack(pady=5)
+            self.search_button.pack(pady=0)
+            self.pack(side="left")
 
         # add title and body fields
         title_entrybox = Labeled_Entrybox(self, text="title")
@@ -30,27 +42,25 @@ class Add_Resource_Window(tk.Frame):
         body_box.pack(pady=10)
 
         self.add_metadata_fields()
+
         for entrybox in self.metadata_entryboxes:
             entrybox.pack(pady=10)
-
-        
         self.submit_button = tk.Button(self, text="Submit", command=self.submit)
         self.submit_button.pack(pady=10)
+
 
     def add_metadata_fields(self):
         # get all metadata fields from the resource template, and have a list with an Entrybox for each
         self.metadata_entryboxes: list[Labeled_Textbox] = []
         metadata_dict = json.loads(self.dropdown.clicked_dict['metadata'])['extra_fields']
         # for each metadata field, add an Entrybox to the list
-        print(metadata_dict)
         for field in metadata_dict:
             entrybox: Labeled_Entrybox = Labeled_Entrybox(self, text=field)
             entrybox.pack(pady=10)
             self.metadata_entryboxes.append(entrybox)
-            print(field)
 
     def fill_info(self, event=None):
-        cas_entry = self.cas_entry.get()# select type of resource
+        cas_entry = self.cas_entry.get()
     def submit(self):
         pass
 class Labeled_Textbox(tk.Frame):
@@ -94,6 +104,7 @@ class Category_Dropdown(tk.Frame):
     def on_category_change(self, *args):
         index: int = self.category_names.index(self.clicked.get())
         self.clicked_dict: dict = self.category_dicts[index]
+        self.parent.draw_items()
 
 
 
@@ -108,6 +119,6 @@ def main():
     root.bind("<Button-5>", scroll_area._on_mousewheel)
 
 
-    Add_Resource_Window(scroll_area.scrollable_frame).pack(side="top", fill="both", expand=True)
+    Add_Resource_Window(scroll_area.scrollable_frame).pack(side="top", padx=20, fill="both", expand=True)
     scroll_area.pack(side="top", fill="both", expand=True)
     root.mainloop()
